b484c34beb22e8047d4308d79a992c490290813e
Shaily Raj <shadowshaily@gmail.com>
2025-10-03 22:07:36 +0530

    Cleaning Up Controllers

--- src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -7,9 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -18,10 +16,6 @@ public class CategoryController {
 
     @Autowired
     private CategoryService categoryService;
-
-//    public CategoryController(CategoryService categoryService) {
-//        this.categoryService = categoryService;
-//    }
 
 //    @GetMapping("/public/categories")
     @RequestMapping(value = "/public/categories", method = RequestMethod.GET)
@@ -39,25 +33,13 @@ public class CategoryController {
 
     @DeleteMapping("/admin/categories/{categoryId}")
     public ResponseEntity<String> deleteCategory(@PathVariable long categoryId) {
-        try {
-            String status = categoryService.deleteCategory(categoryId);
-//            (or)
-//            return new ResponseEntity<>(status, HttpStatus.OK);
-//            (or)
-//            return ResponseEntity.ok(status);
-            return ResponseEntity.status(HttpStatus.OK).body(status);
-        } catch (ResponseStatusException e) {
-            return new ResponseEntity<>(e.getReason(), e.getStatusCode());
-        }
+        String status = categoryService.deleteCategory(categoryId);
+        return new ResponseEntity<>(status, HttpStatus.OK);
     }
 
     @PutMapping("/admin/categories/{categoryId}")
     public ResponseEntity<String> updateCategory(@Valid @RequestBody Category category, @PathVariable Long categoryId) {
-        try {
-            Category savedCategory = categoryService.updateCategory(category, categoryId);
-            return new ResponseEntity<>("Category with category id: "+categoryId+" is updated", HttpStatus.OK);
-        } catch(ResponseStatusException e) {
-            return new ResponseEntity<>(e.getReason(), e.getStatusCode());
-        }
+        Category savedCategory = categoryService.updateCategory(category, categoryId);
+        return new ResponseEntity<>("Category with category id: "+categoryId+" is updated", HttpStatus.OK);
     }
 }
--- src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -1,17 +1,12 @@
 package com.ecommerce.project.service;
 
 import com.ecommerce.project.exceptions.APIException;
-import com.ecommerce.project.exceptions.NoResultFoundException;
 import com.ecommerce.project.exceptions.ResourceNotFoundException;
 import com.ecommerce.project.model.Category;
 import com.ecommerce.project.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -23,10 +18,6 @@ public class CategoryServiceImpl implements CategoryService {
     @Override
     public List<Category> getAllCategories() {
         List<Category> categories = categoryRepository.findAll();
-//        My implementation -> with new custom exception
-//        if(categories.size() == 0) {
-//            throw  new NoResultFoundException("Category is empty!!!");
-//        }
         if(categories.isEmpty()) {
             throw  new APIException("No Category created till now!");
         }
@@ -42,27 +33,9 @@ public class CategoryServiceImpl implements CategoryService {
         categoryRepository.save(category);
     }
 
-//    @Override
-//    public String deleteCategory(Long categoryId) {
-//        List<Category> categories = categoryRepository.findAll();
-//        Category category = categories.stream()
-//                .filter(c -> c.getCategoryId().equals(categoryId))
-//                .findFirst()
-//                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Response not found!"));
-//
-//        categoryRepository.delete(category);
-//        return "Category with categoryId: "+categoryId+ " deleted Successfully!!";
-//    }
-
     @Override
     public String deleteCategory(Long categoryId) {
         Optional<Category> savedCategoryOptional = categoryRepository.findById(categoryId);
-
-//        using pre-defined Exception
-//        Category deleteCategory = savedCategoryOptional
-//                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Response not found"));
-
-        //        using custom Exception
         Category deleteCategory = savedCategoryOptional
                 .orElseThrow(() -> new ResourceNotFoundException("Category", "categoryId", categoryId));
 
@@ -70,33 +43,10 @@ public class CategoryServiceImpl implements CategoryService {
         return "Category with categoryId: "+categoryId+ " deleted Successfully!!";
     }
 
-//    @Override
-//    public Category updateCategory(Category category, Long categoryId) {
-//        List<Category> categories = categoryRepository.findAll();
-//        Optional<Category> optionalCategory = categories.stream()
-//                .filter(c -> c.getCategoryId().equals(categoryId))
-//                .findFirst();
-//
-//        if(optionalCategory.isPresent()) {
-//            Category existingCategory = optionalCategory.get();
-//            existingCategory.setCategoryName(category.getCategoryName());
-//            Category savedCategory = categoryRepository.save(existingCategory);
-//            return existingCategory;
-//        } else {
-//            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found!");
-//        }
-//    }
-
     @Override
     public Category updateCategory(Category category, Long categoryId) {
-//        Optional<Category> savedCategoryOptional = categoryRepository.findById(categoryId);
-
-//        Category savedCategory = categoryRepository.findById(categoryId)
-//                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Response Not Found"));
-
         Category savedCategory = categoryRepository.findById(categoryId)
                 .orElseThrow(() -> new ResourceNotFoundException("Category", "categoryId", categoryId));
-
         category.setCategoryId(categoryId);
         savedCategory = categoryRepository.save(category);
         return savedCategory;
